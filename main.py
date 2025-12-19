e8e3aaf81348163c950b212cd74996c6f7aafd20
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-12-19 21:16:10 +0100

    jai ajouter une fonction qui afiche larbre

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-class Noued: 
+class Noeud: 
     def __init__(self, info) 
         self.info= info #information du noued
         self.fils= []   #listes des fils 
         self.pere= None #pointeur vers le pere papa 
 
+#construction d'un arbre exemple 
 
 def constArbreA(): 
-    r = Noued("R") 
-    a = Noued("A") 
-    b = Noued("B") 
-    c = Noued("C") 
-    d = Noued("D") 
+    r = Noeud("R") 
+    a = Noeud("A") 
+    b = Noeud("B") 
+    c = Noeud("C") 
+    d = Noeud("D") 
 
     r.fils = [a,b]
     a.pere = r 
@@ -21,3 +22,13 @@ def constArbreA():
     d.pere= a 
 
     return r 
+
+
+#affichage d'arbre en parcour profondeur 
+
+def AfficherProfondeur(Noeud, niveau=0):
+    if Noeud is None: 
+        return 
+    print(" " * niveau * 2 + Noeud.info) 
+    for f in Noeud.fils : 
+        AfficherProfondeur(f, niveau+1) 
