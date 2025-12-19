fd08296e27df414401e6dfbcedae5fc2a0d9e217
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-12-19 18:55:19 +0100

    j'ai ajouté des comments

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 class Noued: 
-    def __init__(self, info): 
-        self.info=info 
-        self.fils=[] 
-        self.pere= None
-
+    def __init__(self, info) 
+        self.info= info #information du noued
+        self.fils= []   #listes des fils 
+        self.parent= None #pointeur vers le pere papa 
