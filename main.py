eee3a09f0ce5e2d71a0ec03670bee0e116d261ee
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-12-19 19:01:02 +0100

    j'ai cree les fils et leur parent

--- main.py
+++ main.py
@@ -2,4 +2,22 @@ class Noued:
     def __init__(self, info) 
         self.info= info #information du noued
         self.fils= []   #listes des fils 
-        self.parent= None #pointeur vers le pere papa 
+        self.pere= None #pointeur vers le pere papa 
+
+
+def constArbreA(): 
+    r = Noued("R") 
+    a = Noued("A") 
+    b = Noued("B") 
+    c = Noued("C") 
+    d = Noued("D") 
+
+    r.fils = [a,b]
+    a.pere = r 
+    b.pere = r  
+    
+    a.fils = [c,d]
+    c.pere= a 
+    d.pere= a 
+
+    return r 
